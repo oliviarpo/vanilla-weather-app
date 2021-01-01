70e8da6b1a492b646aaf8ceea60e836c1833b49e
oliviarpo <oliviapovey@outlook.com>
2021-01-01 18:25:40 +0000

    Added current location button

--- src/app.js
+++ src/app.js
@@ -108,6 +108,20 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentLocation);
+}
+
+function currentLocation(position) {
+  let apiKey = "610edcec4f7131569ef97bf1470f78f8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+
+  let apiUrlLocation = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrlLocation).then(displayForecast);
+}
+
 let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
@@ -119,4 +133,7 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+search("Manchester");
